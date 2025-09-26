29735825dea72e69a5e8a20bb84e02af79b92fda
t6nu <74606960+T6nuk@users.noreply.github.com>
2025-09-26 12:57:41 +0300

    Add 3 products with pictures

---
+++ src/data/products.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+        id: 'Tee-gray',
+        name: 'Classic gray t-shirt',
+        desc: 'Soft cotton. Symmetric look. No wrong way',
+        image: '/images/gray_tee.png',
+        vendorUrl: '',
+        price: '29.99',
+    },
+
+    {
+        id: 'Tee_green',
+        name: 'Classic green t-shirt',
+        desc: 'Soft cotton. Symmetric look. No wrong way',
+        image: '/images/green_tee.png',
+        vendorUrl: '',
+        price: '29.99',
+    },
+    {
+        id: 'Tee-white',
+        name: 'Classic white t-shirt',
+        desc: 'Soft cotton. Symmetric look. No wrong way',
+        image: '/images/white_tee.png',
+        vendorUrl: '',
+        price: '29.99',
+    }
+]
